e9da527cf95e246ccb7db6f63f065c85bec97890
Jordan O'Hara <J.O'Hara2@uni.brighton.ac.uk>
2021-05-20 17:15:13 +0100

    updated to include a poster for the customer video player.

--- ts/main.ts
+++ ts/main.ts
@@ -3,8 +3,7 @@ let body : HTMLElement | null = document.body;
 if(body) {
     let videoHeader : HTMLElement | null = document.getElementById('video-header');
     if(videoHeader) {
-        const video : Video = new Video("assets/video/pexels-tea-oebel-6804114.mp4");
+        const video : Video = new Video("assets/video/pexels-tea-oebel-6804114.mp4", "assets/img/video-poster.png");
         videoHeader.append(video.element);
     }
-
 }
