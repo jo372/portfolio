d068a85b26030626f92a1f9122a5b23affb85793
Jordan O'Hara <J.O'Hara2@uni.brighton.ac.uk>
2021-05-22 03:36:02 +0100

    recompiled javascript from typescript files.

--- js/video.js
+++ js/video.js
@@ -1,26 +1,15 @@
-var __assign = (this && this.__assign) || function () {
-    __assign = Object.assign || function(t) {
-        for (var s, i = 1, n = arguments.length; i < n; i++) {
-            s = arguments[i];
-            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
-                t[p] = s[p];
-        }
-        return t;
-    };
-    return __assign.apply(this, arguments);
-};
-var defaultVideoConfiguration = {
+const defaultVideoConfiguration = {
     isLooping: true,
     isPlaying: true
 };
-var Video = /** @class */ (function () {
-    function Video(src, posterSrc, config) {
-        config = __assign({}, defaultVideoConfiguration, config);
+class Video {
+    constructor(src, posterSrc, config) {
+        config = Object.assign({}, defaultVideoConfiguration, config);
         posterSrc = posterSrc || '';
         this.videoStatus = config.isPlaying ? VideoStatus.PLAYING : VideoStatus.PAUSED;
-        var playIcon = document.createElement('i');
+        let playIcon = document.createElement('i');
         this.playIcon = playIcon;
-        var videoElement = document.createElement('video');
+        let videoElement = document.createElement('video');
         videoElement.src = src;
         videoElement.loop = config.isLooping;
         videoElement.poster = posterSrc;
@@ -30,28 +19,28 @@ var Video = /** @class */ (function () {
         this.element = videoElement;
         this.config = config;
     }
-    Video.prototype.getElement = function () {
+    getElement() {
         return this.element;
-    };
-    Video.prototype.play = function () {
+    }
+    play() {
         this.getElement().play();
-    };
-    Video.prototype.pause = function () {
+    }
+    pause() {
         this.getElement().pause();
-    };
-    Video.prototype.resume = function () {
-        var status = this.getStatus();
+    }
+    resume() {
+        let status = this.getStatus();
         if (status === VideoStatus.PAUSED) {
             this.play();
         }
-    };
-    Video.prototype.getVideoConfiguration = function () {
+    }
+    getVideoConfiguration() {
         return this.config;
-    };
-    Video.prototype.addEventListenersTo = function (videoElement) {
-        var _this = this;
+    }
+    addEventListenersTo(videoElement) {
+        let _this = this;
         videoElement.addEventListener('click', function () {
-            var videoStatus = _this.getStatus();
+            let videoStatus = _this.getStatus();
             switch (videoStatus) {
                 case VideoStatus.PAUSED:
                     _this.resume();
@@ -72,20 +61,20 @@ var Video = /** @class */ (function () {
             _this.setStatus(VideoStatus.PAUSED);
         });
         videoElement.addEventListener('ended', function () {
-            var config = _this.getVideoConfiguration();
+            let config = _this.getVideoConfiguration();
             if (!config.isLooping) {
                 _this.setStatus(VideoStatus.STOPPED);
             }
         });
-    };
-    Video.prototype.setStatus = function (status) {
+    }
+    setStatus(status) {
         this.videoStatus = status;
         this.updateVideo();
-    };
-    Video.prototype.getStatus = function () {
+    }
+    getStatus() {
         return this.videoStatus;
-    };
-    Video.prototype.updateVideo = function () {
+    }
+    updateVideo() {
         // do something.
         // console.log(`The Video is currently ${VideoStatus[this.getStatus()]}`);
         // let el : HTMLElement = document.createElement('i');
@@ -103,9 +92,8 @@ var Video = /** @class */ (function () {
                 // something weird happened
                 break;
         }
-    };
-    return Video;
-}());
+    }
+}
 var VideoStatus;
 (function (VideoStatus) {
     VideoStatus[VideoStatus["PLAYING"] = 0] = "PLAYING";
