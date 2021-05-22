56abee4d92212f09fd02c2f3a3ed0aa8ab9da5bb
Jordan O'Hara <J.O'Hara2@uni.brighton.ac.uk>
2021-05-22 16:11:32 +0100

    updated to only change the default event listener, if the element exists.

--- ts/sidenav.ts
+++ ts/sidenav.ts
@@ -1,21 +1,28 @@
 let sideNav : HTMLElement | null = document.querySelector('.rightSidePanel');
+/** if there is a side nav bar */
 if(sideNav) {
+    /** try to get the listed items on the side nav */
     let listItems : NodeListOf<HTMLLIElement> | null = sideNav.querySelectorAll('li');
+
+    /** if there are elements, loop through them all */
     if(listItems) {
+        /** for each of the elements, get their href */
         listItems.forEach((listItem : HTMLLIElement) => {
             let anchorElement : HTMLAnchorElement | null = listItem.querySelector('a');
+            /** if the anchor element exists, add click event */
             if(anchorElement) {
-                anchorElement.addEventListener('click', (e: Event) => { 
-                    e.preventDefault();
-                    return false; 
-                });
-
                 let hrefId : string = anchorElement.hash;
-                listItem.addEventListener('click', function(e: Event) {
-                    e.preventDefault();
-                    if(hrefId !== "") {
+                /** if there is a hash */
+                if(hrefId != "") {
+                    anchorElement.addEventListener('click', (e: Event) => { 
+                        e.preventDefault();
+                        return false; 
+                    });
+                    
+                    listItem.addEventListener('click', function(e: Event) {
                         let el : HTMLElement | null = document.querySelector(hrefId);
                         if(el) {
+                            e.preventDefault();
                             el.scrollIntoView({
                                 behavior: 'smooth', 
                                 block: 'start'
@@ -26,8 +33,8 @@ if(sideNav) {
                                 previousSelectedElement.classList.toggle('active');
                             }
                         listItem.classList.add('active');
-                    }
-                });
+                    });
+                }
             }
         });
     }
