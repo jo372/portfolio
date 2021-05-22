62a3adffc8f505f363440bae7f8936b9e5462951
Jordan O'Hara <J.O'Hara2@uni.brighton.ac.uk>
2021-05-22 03:34:12 +0100

    updated to include the new text typer!

--- js/main.js
+++ js/main.js
@@ -1,8 +1,32 @@
-var body = document.body;
+let body = document.body;
 if (body) {
-    var videoHeader = document.getElementById('video-header');
+    let videoHeader = document.getElementById('video-header');
     if (videoHeader) {
-        var video = new Video("assets/video/pexels-tea-oebel-6804114.mp4", "assets/img/video-poster.png");
+        const video = new Video("assets/video/pexels-tea-oebel-6804114.mp4", "assets/img/video-poster.png");
         videoHeader.append(video.element);
+    }
+    let introTextSection = document.getElementById('intro-text');
+    if (introTextSection) {
+        let container = document.createElement('h1');
+        container.classList.add('text-cursor');
+        introTextSection.append(container);
+        const typer = new TextTyper(container, 30, 100);
+        // minTypingTime?: number, randomTypingTime
+        let htmlString = "Hello, I am <span><a href=\"https://www.linkedin.com/in/jordan-o-hara-875b841a9/\" target=\"_blank\">Jordan O'Hara</a></span>. I'm a fulltime Software Engineering Student @ Brighton University";
+        // let textToType : string = "Hello, I am Jordan O'Hara. I'm a fulltime Software Engineering Student @ Brighton University";
+        typer
+            .clearNow()
+            .wait(1000)
+            .type("Hi,")
+            .remove("Hi,".length)
+            .type("Hello, I am Jordan O'Hara.")
+            .type(" I'm a fulltime Software Engineering Student")
+            .type(" @ ")
+            .type("Brighton University")
+            .stop()
+            .chain(() => {
+            // container.classList.remove('text-cursor');
+            container.innerHTML = htmlString;
+        });
     }
 }
